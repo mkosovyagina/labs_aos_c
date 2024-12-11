99d254aa8afd9a8dd8cc4a6d31cd8bc7e100cb5b
mkosovyagina <34448111+mkosovyagina@users.noreply.github.com>
2024-12-11 15:00:59 +0300

    Update listing_1.c

--- lab_5/listing_1.c
+++ lab_5/listing_1.c
@@ -1,62 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <sys/errno.h>
-#include <signal.h>
+//#include <stdint.h>
+//#include <unistd.h>
+#include <math.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <memory.h>
+#include <string.h>
+#include <malloc.h>
+//#include <assert.h>
+//#include <time.h>
+//#include <pthread.h>
+//#include <sys/time.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
+
+struct message_simple {
+    long type;
+    char payload[100];
+};
 
 int main() {
-    pid_t pid, parent_pid, child_pid, child_pid_finished;
-    int	child_ppid, child_pgid, parent_ppid, parent_pgid;
-    int status, i, j, pipe_status;
-    int file_descriptors_pipe[2];
-    char message[] = "Hello, World!\nHave a nice day!!!\nWelcome to MEPHI! ;)";
-    int message_len = sizeof(message);
-    char read_buffer[message_len];
+    int res;
     
-    pipe_status = pipe(file_descriptors_pipe);
-    
-    if(pipe_status < 0){
-    	printf("Pipe creation failed");}
-    
-    pid = fork();
-    printf("Current pid %d\n", pid);
-    if (pid == -1){
-    perror("Failed to create child process");}
-    
-    if (pid > 0){
-    
-    parent_pid = getpid();
-    parent_ppid = getppid();
-    parent_pgid = getpgid(parent_pid);
-    printf("Parent process executes, pid = %d\n", parent_pid);
-    printf("Parent process parent pid = %d\n", parent_ppid);
-    printf("Parent process group id = %d\n", parent_pgid);
-    //close(file_descriptors_pipe[0]);
-    write(file_descriptors_pipe[1], message, message_len);
-    
-    child_pid_finished = wait(&status);
-    close(file_descriptors_pipe[1]);
-    
-    printf("Child process %d successfully executed with code %d\n and status %d", child_pid_finished, WEXITSTATUS(status), status);
-    exit(0);
+    int id = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+    if (id == -1) {
+        perror("Failed to create message queue");
+        exit(EXIT_FAILURE);
     }
-    else{
-    child_pid = getpid();
-    child_ppid = getppid();
-    child_pgid = getpgid(child_pid);
-    printf("Child process executes, pid = %d\n", child_pid);
-    printf("Child process parent pid = %d\n", child_ppid);
-    printf("Child process group id = %d\n", child_pgid);
-    //close(file_descriptors_pipe[1]);
-    printf("Child process is readind from pipe\n");
-    read(file_descriptors_pipe[0], &read_buffer, message_len);
-    printf("%s\n", read_buffer);
-    close(file_descriptors_pipe[0]);
-    
-    exit(0);
-    }
+    printf("queue id %d\n", id);
+  
+    struct message_simple msg;
+    msg.type = 2;
+    strcpy((char*)&msg.payload, "Hello!");
 
+    do {  
+        res = msgsnd(id, &msg, sizeof(msg), 0);
+        if (res == -1) {
+            if (errno == EINTR) continue;
+            perror("Failed to send message");
+            msgctl(id, IPC_RMID, NULL);
+            exit(EXIT_FAILURE);
+        }   
+    } while (res == -1);
+    
+    printf("sent\n");
+    
+    msg.type = 45;
+    strcpy((char*)&msg.payload, "Welcome!");
+    
+    do {
+        res = msgsnd(id, &msg, sizeof(msg), IPC_NOWAIT);
+        if (res == -1) {
+            if (errno == EINTR) continue;
+            perror("Failed to send message");
+            msgctl(id, IPC_RMID, NULL);
+            exit(EXIT_FAILURE);
+        }
+    } while (res == -1);
+    
+    printf("sent\n");
+    
+    msg.type = 966789;
+    strcpy((char*)&msg.payload, "&&&&&7777777777777777777444444!");
+    
+    do {
+        res = msgsnd(id, &msg, sizeof(msg), IPC_NOWAIT);
+        if (res == -1) {
+            if (errno == EINTR) continue;
+            perror("Failed to send message");
+            msgctl(id, IPC_RMID, NULL);
+            exit(EXIT_FAILURE);
+        }
+    } while (res == -1);
+    
+    printf("sent\n");
+    
+    struct msqid_ds stat;
+        
+    res = msgctl(id, IPC_STAT, &stat);
+    if (res == -1) {
+        perror("Failed to get statistics");
+        msgctl(id, IPC_RMID, NULL);
+        exit(EXIT_FAILURE);
+    }
+    printf("msg_cbytes %lu\n", stat.msg_cbytes);
+    printf("msg_qnum %lu\n", stat.msg_qnum);
+    printf("msg_qbytes %lu\n", stat.msg_qbytes);
+    
+    res = msgctl(id, IPC_RMID, NULL);
+    if (res == -1) {
+        perror("Failed to remove queue");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
 }
